e03e9df2734e85c6fef8f9fd3504f92737a5df2f
Anastasia <61227061+AreYouPretty@users.noreply.github.com>
2020-09-06 14:40:25 +1200

    Отображение набора карт. Программмная загрузка изображений.

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] private GameObject cardBack;
+    
+    
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private SceneController controller;
+    [SerializeField] private MemoryCard originalCard;
+    [SerializeField] private Sprite[] images;
+
+    private int _id;
+
+    void Start()
+    {
+        int id = Random.Range(0, images.Length);
+        originalCard.SetCard(id, images[id]);
+    }
+
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+}
