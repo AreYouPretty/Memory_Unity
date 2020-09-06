0144f2b9e1497cb60f6e04cf0bfc5b9818d110bb
Anastasia <61227061+AreYouPretty@users.noreply.github.com>
2020-09-07 05:33:02 +1200

    Выбор изображения из компонентов SceneController

--- Assets/Scripts/MemoryCard.cs
+++ Assets/Scripts/MemoryCard.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class MemoryCard : MonoBehaviour
 {
     [SerializeField] private GameObject cardBack;
-    
-    
+    [SerializeField] private SceneController controller; 
     public void OnMouseDown()
     {
         if (cardBack.activeSelf)
         {
             cardBack.SetActive(false);
         }
+    }
+
+    private int _id;
+
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
     }
 }
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
-    [SerializeField] private SceneController controller;
     [SerializeField] private MemoryCard originalCard;
     [SerializeField] private Sprite[] images;
 
@@ -14,16 +13,5 @@ public class SceneController : MonoBehaviour
     {
         int id = Random.Range(0, images.Length);
         originalCard.SetCard(id, images[id]);
-    }
-
-    public int id
-    {
-        get { return _id; }
-    }
-
-    public void SetCard(int id, Sprite image)
-    {
-        _id = id;
-        GetComponent<SpriteRenderer>().sprite = image;
     }
 }
