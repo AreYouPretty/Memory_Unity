2c3f7b8f129d218a16ad6a5b9cf04683c7704351
Anastasia <61227061+AreYouPretty@users.noreply.github.com>
2020-09-07 06:10:59 +1200

    Экземпляры карт

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
+    public const int gridRows = 2;
+    public const int gridCols = 4;
+    public const float offsetX = 2.0f;
+    public const float offsetY = 2.5f;
+
     [SerializeField] private MemoryCard originalCard;
     [SerializeField] private Sprite[] images;
 
@@ -11,7 +16,30 @@ public class SceneController : MonoBehaviour
 
     void Start()
     {
-        int id = Random.Range(0, images.Length);
-        originalCard.SetCard(id, images[id]);
+        Vector3 startPos = originalCard.transform.position;
+
+        for (int i = 0; i < gridCols; i++) 
+        {
+            for (int j = 0; j < gridRows; j++)
+            {
+                MemoryCard card;
+                if (i == 0 && j == 0)
+                {
+                    card = originalCard;
+                }
+                else 
+                {
+                    card = Instantiate(originalCard) as MemoryCard;
+                }
+                
+                int id = Random.Range(0, images.Length);
+                card.SetCard(id, images[id]);
+
+                float posX = (offsetX * i) + startPos.x;
+                float posY = -(offsetY * j) + startPos.y;
+                card.transform.position = new Vector3(posX, posY, startPos.z);
+            }
+        }
+        
     }
 }
